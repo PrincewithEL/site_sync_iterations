353a8c22599f012c1005eb6977aa518887639185
PrincewithEL <86285043+PrincewithEL@users.noreply.github.com>
2024-11-09 06:11:11 +0300

    Sem 2 - Project #50 Corrections to the All APIs #1

--- sitesyncApp/views.py
+++ sitesyncApp/views.py
@@ -70,6 +70,7 @@ import re
 from collections import Counter
 from django.utils.dateformat import DateFormat
 from django.utils.formats import date_format
+from datetime import timedelta
 
 # Download NLTK stopwords if not already downloaded
 nltk.download("stopwords")
@@ -1239,7 +1240,8 @@ def chat_room_view(request, pk):
         receiver_profile = get_object_or_404(Profile, user_id=receiver_user_id) if receiver_user_id else None
         receiver_user_type = receiver_profile.user_type if receiver_profile else None
 
-        formatted_timestamp = date_format(message.timestamp, format='Y-m-d H:i:s')
+        adjusted_timestamp = message.timestamp + timedelta(hours=3)
+        formatted_timestamp = date_format(adjusted_timestamp, format='Y-m-d H:i:s')
 
         sender_user = User.objects.get(id=message.sender_user_id)
         sender_first_name = sender_user.first_name
