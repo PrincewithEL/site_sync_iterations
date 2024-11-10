4990603d217fac02eefbc79adc766a3e4bc12c5b
PrincewithEL <86285043+PrincewithEL@users.noreply.github.com>
2024-11-10 11:35:51 +0300

    Sem 2 - Project #50 Corrections to the All APIs #12

--- sitesyncApp/views.py
+++ sitesyncApp/views.py
@@ -1242,12 +1242,8 @@ def chat_room_view(request, pk):
         receiver_profile = get_object_or_404(Profile, user_id=receiver_user_id) if receiver_user_id else None
         receiver_user_type = receiver_profile.user_type if receiver_profile else None
 
-        if message.updated_at:
-            adjusted_timestamp = message.updated_at + timedelta(hours=3)
-            formatted_timestamp = "Edited At: " + date_format(adjusted_timestamp, format='Y-m-d H:i:s')
-        else:
-            adjusted_timestamp = message.timestamp + timedelta(hours=3)
-            formatted_timestamp = date_format(adjusted_timestamp, format='Y-m-d H:i:s')
+        adjusted_timestamp = message.timestamp + timedelta(hours=3)
+        formatted_timestamp = date_format(adjusted_timestamp, format='Y-m-d H:i:s')
 
         sender_user = User.objects.get(id=message.sender_user_id)
         sender_first_name = sender_user.first_name
